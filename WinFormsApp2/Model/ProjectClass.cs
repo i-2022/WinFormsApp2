59d4d5563c99ed8307f24396272324ccbe0c322d
i-2022 <Programiranje2022>
2022-08-23 17:07:33 +0200

    Add project files.

---
+++ WinFormsApp2/Model/ProjectClass.cs
@@ -0,0 +1,22 @@
+﻿
+namespace WinFormsApp2.Model
+{
+    public class Text1
+    {
+        public int TextID { get; set; }
+        public string? TextString { get; set; }
+    }
+    public class FileText
+    {
+        public string? TextString { get; set; }
+    }
+    public class LoginModel
+    {
+        public string? Username { get; set; }
+        public string? Password { get; set; }
+    }
+    public class TextModel
+    {
+        public string? Tekst { get; set; }
+    }
+}
